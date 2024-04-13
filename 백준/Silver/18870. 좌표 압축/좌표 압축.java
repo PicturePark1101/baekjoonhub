778d9fd8b03798f9781a744a04ea3037942e479a
PicturePark <109809242+PicturePark1101@users.noreply.github.com>
2024-04-13 10:07:54 +0900

    [Silver II] Title: 좌표 압축, Time: 2480 ms, Memory: 365472 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+
+    ArrayList<Integer> originList = new ArrayList<>();
+
+    Map<Integer, Integer> linkedHashMap = new LinkedHashMap<>();
+    Set<Integer> set = new LinkedHashSet<>();
+
+    int N = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+
+    for (int i = 0; i < N; i++) {
+      int num = Integer.parseInt(st.nextToken());
+      originList.add(num);
+      set.add(num);
+    }
+
+    ArrayList<Integer> newList = new ArrayList<>(set);
+    newList.sort(Comparator.naturalOrder());
+
+    for (int i = 0; i < newList.size(); i++) {
+      if (!linkedHashMap.containsKey(newList.get(i))) {
+        linkedHashMap.put(newList.get(i), i);
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      bw.write(String.valueOf(linkedHashMap.get(originList.get(i)))+" ");
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+
+}
