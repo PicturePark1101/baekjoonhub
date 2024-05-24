0a5d024dc5f77ed087c46e11432491295671443e
PicturePark <109809242+PicturePark1101@users.noreply.github.com>
2024-05-24 14:10:43 +0900

    [Bronze I] Title: 단어 공부, Time: 256 ms, Memory: 20032 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,38 +1,52 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
-
-  public static String answer(int[] alphabet) {
-
-    char ans = 0;
-    int maxCnt = -1;
-
-    for (int i = 0; i < alphabet.length; i++) {
-      if (alphabet[i] > maxCnt) {
-        ans = (char) (i + 'A');
-        maxCnt = alphabet[i];
-      } else if (alphabet[i] == maxCnt){
-        ans = '?';
-      }
-    }
-
-    return String.valueOf(ans);
-  }
-  public static void main(String[] args) throws Exception {
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    int[] alphabet = new int[26];
-
-    String input = br.readLine().toUpperCase();
     
-    for (int i = 0; i < input.length(); i++) {
-      alphabet[input.charAt(i) - 'A']++;
+    public static int[] cntArr(String arr) {
+        int[] cntAlpha = new int[26];
+        
+        for (int i = 0; i < arr.length(); i++) {
+            char ch = arr.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                cntAlpha[ch - 'a']++;
+            } else {
+                cntAlpha[ch - 'A']++;  
+            }
+        }
+        return cntAlpha;
     }
+    
+    public static char maxAlpha(int[] cntAlpha) {
+        
+        int max = Integer.MIN_VALUE;
+        int idx = 0;
+        
+        for (int i = 0; i < 26; i++) {
+            if (max < cntAlpha[i]) {
+                max = cntAlpha[i];
+                idx = i;
+            }
+        }
+        
+        char ans = (char)(idx + 'A');
+        for (int i = 0; i < 26; i++) {
+            if (max == cntAlpha[i] && idx != i) {
+                ans = '?';
+                break;
+            }
+        }
+        return ans;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    bw.write(answer(alphabet));
-
-    br.close();
-    bw.close();
-  }
+        int[] alphaCnt = cntArr(br.readLine());
+        
+        System.out.println(maxAlpha(alphaCnt));
+        br.close();
+    }
+    
 }
