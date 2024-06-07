9c402454ce9d9e3f0694488a69b3365a70e5fdbc
PicturePark <109809242+PicturePark1101@users.noreply.github.com>
2024-06-07 16:07:17 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 148 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.java
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.java
@@ -1,36 +1,30 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-  public static void main(String[] args) throws Exception {
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    int N = Integer.parseInt(br.readLine());
-
-    for (int i = 0 ; i < N; i++) {
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      int min = Integer.MAX_VALUE;
-      int sum = 0;
-
-      for (int j = 0; j < 7; j++) {
-        int num = Integer.parseInt(st.nextToken());
-        if (num % 2 == 0) {
-          sum += num;
-          if (min > num) {
-            min = num;
-          }
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < T; i++) {
+            int min = Integer.MAX_VALUE; 
+            int sum = 0;
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 7; j++) {
+                int n = Integer.parseInt(st.nextToken());
+                if (n % 2 == 0) {
+                    sum += n;
+                    min = Math.min(min, n);
+                }
+            }
+            sb.append(sum).append(" ").append(min).append("\n");
         }
-      }
-      bw.write(String.valueOf(sum)+ " " +String.valueOf(min)+"\n");
-
+        System.out.println(sb);
+        br.close();
     }
-
-    bw.flush();
-    bw.close();
-    br.close();
-
-  }
 }
