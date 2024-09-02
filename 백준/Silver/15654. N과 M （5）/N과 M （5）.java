20f7b67228e1fbd2501033a17393d914ff15daec
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-09-02 19:54:31 +0900

    [Silver III] Title: N과 M (5), Time: 2016 ms, Memory: 83308 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,53 +1,51 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    
+    private static int[] nums;
+    private static int[] bucket;
+    private static boolean[] visited;
+    private static int N;
+    private static int M;
 
-  private static int[] bucket;
-  private static int[] nums;
-  private static boolean[] visited;
-  // 순열, 중복 x
-  public static void pick(int[] nums, int N, int M, int depth) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-    if (M == depth) {
-      for (int i = 0; i < M; i++) {
-        System.out.print(bucket[i]+" ");
-      }
-      System.out.println();
-      return;
+        nums = new int[N];
+        bucket = new int[M];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        
+        dfs(0);
+        br.close();    
     }
+    
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(bucket[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+        
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                bucket[depth] = nums[i];
+                visited[i] = true;
+                dfs(depth + 1);
+                visited[i] = false;
 
-    for (int i = 0; i < N; i++) {
-      if (!visited[i]) {
-        visited[i] = true;
-        bucket[depth] = nums[i];
-        pick(nums, N, M, depth + 1);
-        visited[i] = false;
-      }
+            }
+        }
     }
-  }
-
-  public static void main(String[] args) throws Exception {
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-
-    int N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
-
-    nums = new int[N];
-    bucket = new int[M];
-    visited = new boolean[N];
-
-    st = new StringTokenizer(br.readLine());
-
-    for (int i = 0; i < N; i++) {
-      nums[i] = Integer.parseInt(st.nextToken());
-    }
-
-    Arrays.sort(nums);
-    pick(nums, N, M, 0);
-  }
 }
