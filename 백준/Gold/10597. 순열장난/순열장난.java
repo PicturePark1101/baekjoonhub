726d74aa9a1f1dc1ea54481d54111b5ed4a20004
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-09-03 17:06:01 +0900

    [Gold V] Title: 순열장난, Time: 112 ms, Memory: 15056 KB -BaekjoonHub

--- 백준/Gold/10597. 순열장난/순열장난.java
+++ 백준/Gold/10597. 순열장난/순열장난.java
@@ -1,12 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
   private static int[] bucket;
   private static boolean[] visited;
-
   private static StringBuilder sb;
   private static int[] str;
   private static boolean findAns;
@@ -19,15 +17,14 @@ public class Main {
     str = new int[inputStr.length];
     bucket = new int[inputStr.length];
     visited = new boolean[51];
+    sb = new StringBuilder();
 
     for (int i = 0; i < str.length; i++) {
       str[i] =  Integer.parseInt(inputStr[i]);
     }
 
-    sb = new StringBuilder();
     makeArr(0, 0);
     System.out.println(sb);
-
     br.close();
   }
 
@@ -38,49 +35,33 @@ public class Main {
     }
 
     if (depth >= str.length) {
-      // printBucket(size);
-      if (isValid(bucket.clone(), size)) {
-        findAns = true;
-        for (int i = 0; i < size; i++) {
-          sb.append(bucket[i]).append(" ");
-        }
-        sb.append("\n");
+      for (int i = 1; i <= size; i++) {
+        if (!visited[i]) return;
       }
+      findAns = true;
+      for (int i = 0; i < size; i++) {
+        sb.append(bucket[i]).append(" ");
+      }
+      sb.append("\n");
       return;
     }
 
-    if (depth + 1 != str.length) { // 마지막 배열이 아니라면
-      int tmp = str[depth] * 10 + str[depth + 1];
-      if (tmp <= 50 &&  !visited[tmp])  { // 50 이하면 탐색하러감. 아니면 안감....
-        bucket[size] = str[depth] * 10 + str[depth + 1];
-        visited[tmp] = true;
-        makeArr(depth + 2, size + 1);
-        visited[tmp] = false;
+    int num = str[depth];
+    if (num != 0 && !visited[num]) {
+      bucket[size] = num;
+      visited[num] = true;
+      makeArr(depth + 1, size + 1);
+      visited[num] = false;
+    }
 
+    if (depth + 1 != str.length) {
+      num = str[depth] * 10 + str[depth + 1];
+      if (num <= 50 &&  !visited[num])  {
+        bucket[size] = num;
+        visited[num] = true;
+        makeArr(depth + 2, size + 1);
+        visited[num] = false;
       }
     }
-
-    int tmp2 = str[depth];
-    if (tmp2 != 0 && !visited[tmp2]) {
-      bucket[size] = str[depth];
-      visited[tmp2] = true;
-      makeArr(depth + 1, size + 1);
-      visited[tmp2] = false;
-    }
-  }
-
-  private static void printBucket(int size) {
-    for (int i = 0; i < size; i++) {
-      System.out.print(bucket[i] + " ");
-    }
-    System.out.println();
-  }
-
-  private static boolean isValid(int[] validBucket, int size) {
-    Arrays.sort(validBucket, 0, size);
-    for (int i = 1; i <= size; i++) {
-      if (validBucket[i - 1] != i){ return false; };
-    }
-    return true;
   }
 }
