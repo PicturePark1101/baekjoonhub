e6c2cc773f716f90eb5632e310c332eb1e1ed136
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-09-19 23:23:15 +0900

    [level 3] Title: 단어 변환, Time: 0.51 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    private boolean[] visited;
+    private int answer;
+    
+    public int solution(String begin, String target, String[] words) {
+        answer = Integer.MAX_VALUE;
+        
+        visited = new boolean[words.length];
+        
+        if (existTarget(words, target)) {
+            dfs(words, 0, target, begin);
+            return answer;
+        } {
+            return 0;
+        }
+    }
+        
+    private boolean existTarget(String[] words, String target) {
+        for (int i = 0; i < words.length; i++) { 
+            if (target.equals(words[i])) return true;
+        }
+        return false;
+    }
+    
+    private void dfs(String[] words, int depth, String target, String current)  {
+        if (target.equals(current)) {
+            answer = Math.min(answer, depth);
+            // System.out.println("answer : " +answer);
+            return;
+        }
+        
+        if (depth == words.length) {
+            return;
+        }
+                
+        for (int i = 0; i < words.length; i++) {
+            //방문하는 조건은 한글자만 다르고, 방문하지 않은 경우임
+            if (differentOneWord(current, words[i]) && !visited[i]) {
+                // System.out.println("방문티비 "+words[i]+ " 깊이" +depth);
+                visited[i] = true;
+                dfs(words, depth + 1, target, words[i]);
+                visited[i] = false;
+                // System.out.println("돌아옴 ");
+                // System.out.println();
+
+            }
+        }        
+    }
+    
+    private boolean differentOneWord(String str1, String str2) {
+        int count = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) count++;
+        }
+
+        return count == 1;
+    }
+}
