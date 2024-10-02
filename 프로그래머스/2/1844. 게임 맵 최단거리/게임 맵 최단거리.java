0de2dfcb563dd8f0e486f151c1b2cc3fa7e9105a
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-10-02 17:24:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.53 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,69 +1,66 @@
 import java.util.*;
 
-class Solution {
 
-    private boolean[][] visited;
-    private int n;
-    private int m;
+
+class Solution {
+    
+    static class Map {
+        private int r;
+        private int c;
+    
+        public Map(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+    
+    private static boolean[][] visited;
+    private static int[] dx = { -1, 0, 0, 1 };
+    private static int[] dy = { 0, 1, -1, 0 };
+    private static int n;
+    private static int m;
     
     public int solution(int[][] maps) {
         n = maps.length;
-        m = maps[0].length;
-        
+        m = maps[0].length;    
         visited = new boolean[n][m];
         return bfs(maps);
     }
     
     private int bfs(int[][] maps) {
-        // 큐..
-        int[] dx = {-1, 0, 0, 1};
-        int[] dy = {0, 1, -1, 0};
-        Queue<Board> queue = new LinkedList<>();
-        
-        queue.offer(new Board(0, 0));
+        Queue<Map> queue = new ArrayDeque<>();
+        int length = 0;
+        queue.add(new Map(0, 0));
         visited[0][0] = true;
-        int level = 0;
         
         while(!queue.isEmpty()) {
-            level++;   
+          length++;
             int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Board tempBoard = queue.poll();
-                if (tempBoard.getR() == n - 1 && tempBoard.getC() == m - 1) return level;
-                
-                for (int j = 0; j < 4; j++) {
-                    int newR = tempBoard.getR() + dx[j];
-                    int newC = tempBoard.getC() + dy[j];
-                    // 방문할 수 있는 길이라면 
-                    if (validPath(newR, newC) && !visited[newR][newC] && maps[newR][newC] == 1) {
-                        queue.offer(new Board(newR, newC));
-                        visited[newR][newC] = true;
-                    }
+           for (int j = 0; j < size; j++) {
+                Map map = queue.poll();
+                int mR = map.r;
+                int mC = map.c;
+               
+                if (mR == n - 1 && mC == m - 1) {
+                    return length;
+                }
+
+                visited[mR][mC] = true;
+                for (int i = 0; i < 4; i++) {
+                    int newR = dx[i] + mR;
+                    int newC = dy[i] + mC;
+                    if (validPath(newR, newC) && maps[newR][newC] == 1 && !visited[newR][newC]) {
+                        queue.add(new Map(newR, newC));
+                        visited[newR][newC] = true;                    
+                    }          
                 }
             }
         }
+        
         return -1;
     }
     
-    private boolean validPath(int r, int c) {
+    private boolean validPath(int r, int c){
         return r >= 0 && r < n && c >= 0 && c < m;
-    }
-}
-
-class Board {
-    private int r;
-    private int c;
-    
-    public Board (int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
-    
-    public int getR() {
-        return r;
-    }
-    
-    public int getC() {
-        return c;
     }
 }
