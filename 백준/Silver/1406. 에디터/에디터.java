c8bd12a90cf84f8ba06b61efa4ce8d053704d26f
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-10-14 13:13:24 +0900

    [Silver II] Title: 에디터, Time: 636 ms, Memory: 127608 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -2,50 +2,52 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-
-    LinkedList<String> linkedList = new LinkedList<>(Arrays.asList(br.readLine().split("")));
+    List<String> str = Arrays.asList(br.readLine().split(""));
+    Stack<String> stack1 = new Stack<>();
+    Stack<String> stack2 = new Stack<>();
+    stack1.addAll(str);
 
     int N = Integer.parseInt(br.readLine());
-
-    ListIterator<String> iter = linkedList.listIterator();
-    //커서 초기화 : 맨 뒤
-    while(iter.hasNext()) {
-      iter.next();
+    for (int i = 0; i < N; i++) {
+      executeCommand(stack1, stack2, br.readLine());
     }
+    while (!stack2.isEmpty()) {
+      stack1.add(stack2.pop());
+    }
+    StringBuilder sb = new StringBuilder();
+    while (!stack1.isEmpty()) {
+      sb.append(stack1.pop());
+    }
+    System.out.println(sb.reverse());
+  }
 
-    while (N-- > 0) {
-      st = new StringTokenizer(br.readLine());
-      String command = st.nextToken();
-
-      switch (command) {
-        case "B" :
-          if (iter.hasPrevious()) {
-            iter.previous();
-            iter.remove();
-          }
-          break;
-        case "D" :
-          if(iter.hasNext()) {
-            iter.next();
-          }
-          break;
-        case "L" :
-          if(iter.hasPrevious()) {
-            iter.previous();
-          }
-          break;
-        case "P" :
-          iter.add(st.nextToken());
-          break;
+  private static void executeCommand(Stack<String> stack1, Stack<String> stack2, String command) {
+    StringTokenizer st = new StringTokenizer(command);
+    switch(st.nextToken()) {
+      case "L" : {
+        if (!stack1.isEmpty()) {
+          stack2.add(stack1.pop());
+        }
+        break;
+      }
+      case "D" : {
+        if (!stack2.isEmpty()) {
+          stack1.add(stack2.pop());
+        }
+        break;
+      }
+      case "B" : {
+        if (!stack1.isEmpty()) {
+          stack1.pop();
+        }
+        break;
+      }
+      case "P" : {
+        stack1.add(st.nextToken());
+        break;
       }
     }
-
-    System.out.println(String.join("", linkedList));
-    br.close();
   }
 }
