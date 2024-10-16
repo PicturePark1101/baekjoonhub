8116f9dc3b4df2082dc3b96303a69b533c80654e
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-10-16 20:37:20 +0900

    [Silver II] Title: 스택 수열, Time: 312 ms, Memory: 29360 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,38 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-  public static void main (String[] args) throws Exception {
-
+  public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
-
-    int N = Integer.parseInt(br.readLine());
     Stack<Integer> stack = new Stack<>();
-    
-    int last = 1;
-    for (int i = 0; i < N; i++) {
-      int n = Integer.parseInt(br.readLine());
-      if (n >= last) {
-        for (int j = last; j <= n; j++) {
-          stack.push(j);
-          sb.append("+").append("\n");
-        }
-        last = n + 1;
-      }
-      int peek = stack.peek();
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
 
-      if (peek != n) {
-        sb = new StringBuilder();
-        sb.append("NO");
+    int pointer = 1;
+    for (int i = 0; i < N; i++) {
+      pointer = executeCommand(stack, pointer, sb, Integer.parseInt(br.readLine()));
+      if (pointer == -1) {
         break;
       }
-      stack.pop();
-      sb.append("-").append("\n");
     }
+
     System.out.println(sb);
     br.close();
+  }
+
+  private static int executeCommand(Stack<Integer> stack, int pointer, StringBuilder sb, int inputN) {
+    if (stack.isEmpty() || stack.peek() < inputN) {
+      while (inputN >= pointer) {
+        stack.push(pointer++);
+        sb.append("+").append("\n");
+      }
+      sb.append("-").append("\n");
+      stack.pop();
+    } else {
+      if (stack.peek() == inputN) {
+        stack.pop();
+        sb.append("-").append("\n");
+      } else {
+        sb.setLength(0);
+        sb.append("NO");
+        return -1;
+      }
+    }
+    return pointer;
   }
 }
