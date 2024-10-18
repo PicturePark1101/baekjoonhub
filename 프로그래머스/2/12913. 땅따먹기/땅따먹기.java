d2b1a0d6d0a14dd85a2dbc02ea5e63eebd56e40c
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-10-18 14:03:57 +0900

    [level 2] Title: 땅따먹기, Time: 16.48 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,28 @@
+class Solution {
+    int solution(int[][] land) {
+        int N = land.length;
+        
+        int[][] dp = new int[N][4];
+        
+        for (int i = 0; i < 4; i++) {
+            dp[0][i] = land[0][i];
+        }
+        
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < 4; j++) {           
+                for (int k = 0; k < 4; k++) {
+                    if (k != j) {
+                        dp[i][j] = Math.max(dp[i][j], land[i][j] + dp[i - 1][k]);
+                    }
+                } 
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, dp[N - 1][i]);
+        }
+        
+        return answer;
+    }
+}
