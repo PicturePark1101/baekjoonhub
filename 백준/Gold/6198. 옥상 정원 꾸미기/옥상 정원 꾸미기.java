cba5d6618550613683c55bd2d03d66a178094487
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-10-19 18:10:38 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 244 ms, Memory: 24888 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Stack<Integer> stack = new Stack<>();
+    int N = Integer.parseInt(br.readLine());
+    long[] arr = new long[N];
+    long[] count = new long[N];
+
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+    }
+    stack.push(0);
+
+    for (int i = 1; i < N; i++) {
+      while ((!stack.isEmpty()) && arr[stack.peek()] <= arr[i]) {
+        int popIndex = stack.pop();
+        count[popIndex] = i - popIndex - 1;
+      }
+      stack.push(i);
+    }
+
+    while (!stack.isEmpty()) {
+      int popIndex = stack.pop();
+      count[popIndex] = N - popIndex - 1;
+    }
+
+    long answer = 0L;
+    for (long n : count) {
+      answer += n;
+    }
+    System.out.println(answer);
+    br.close();
+  }
+}
