829f49369c8bcad088b4d564e8f7a902f1bb536c
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-10-19 19:15:53 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 28936 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new ArrayDeque<>();
+        
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+        
+        while (queue.size() != 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+        br.close();
+    }
+    
+    
+}
