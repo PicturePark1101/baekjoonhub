02199314c3f5f1c651cd05e121c008cdfbcb7439
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-10-21 11:13:48 +0900

    [Gold V] Title: AC, Time: 696 ms, Memory: 94984 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,91 +1,74 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+  private static StringBuilder sb = new StringBuilder();
 
-  public static void queue(String[] nums, String command) {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
 
-    Deque<String> qu = new LinkedList<>();
-
-    for (int i = 0; i < nums.length; i++) {
-      qu.offer(nums[i]);
+    for (int i = 0; i < T; i++) {
+      String p = br.readLine();
+      int n = Integer.parseInt(br.readLine());
+      executeCommand(p, makeNumsArr(br.readLine(), n));
     }
+    System.out.println(sb);
+    br.close();
+  }
 
-    boolean isReverse = false;
+  private static void executeCommand(String p, Deque<Integer> arr) {
+    boolean isFirstPointer = true;
 
-    for (int i = 0; i < command.length(); i++) {
-      char ch = command.charAt(i);
-
-      if (ch == 'R') {
-        isReverse = !isReverse;
-      } else {
-        if (qu.isEmpty()) {
-          System.out.println("error");
+    for (int i = 0; i < p.length(); i++) {
+      if (p.charAt(i) == 'R') { // 순서 뒤집기
+        isFirstPointer = !isFirstPointer;
+      } else{
+        if (arr.isEmpty()) {
+          sb.append("error\n");
           return;
         } else {
-          if (isReverse) {
-            qu.pollLast();
+          if (isFirstPointer) {
+            arr.pollFirst();
           } else {
-            qu.pollFirst();
+            arr.pollLast();
           }
         }
       }
     }
-    printDeque(qu, isReverse);
+
+    printArr(arr, isFirstPointer);
   }
 
-  public static void printDeque(Deque<String> queue, boolean isReverse) {
-
-    StringBuilder sb = new StringBuilder();
+  private static void printArr(Deque<Integer> arr, boolean isFirstPointer) {
     sb.append("[");
 
-    while (!queue.isEmpty()) {
-      if (isReverse) {
-        sb.append(queue.pollLast());
+    while (!arr.isEmpty()) {
+      if (isFirstPointer) {
+        sb.append(arr.pollFirst());
       } else {
-        sb.append(queue.pollFirst());
+        sb.append(arr.pollLast());
       }
-      if (!queue.isEmpty()) {
+      if (!arr.isEmpty()) {
         sb.append(",");
       }
     }
+
     sb.append("]\n");
-    System.out.print(sb);
   }
-  public static void main(String[] args) throws Exception {
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringBuffer sb = new StringBuffer();
-
-    int T = Integer.parseInt(br.readLine());
-
-    for (int i = 0; i < T; i++) {
-      sb = new StringBuffer();
-
-      String p = br.readLine();
-      int N = Integer.parseInt(br.readLine());
-
-      String arrStr = br.readLine();
-      sb.append(arrStr);
-
-      sb.deleteCharAt(arrStr.length() - 1);
-      sb.deleteCharAt(0);
-
-      String[] nums = new String[N];
-      if (sb.isEmpty()) {
-        nums = new String[]{};
-      } else {
-        nums = sb.toString().split(",");
-      }
-      queue(nums, p);
+  private static Deque<Integer> makeNumsArr(String s, int n) {
+    s = s.substring(1, s.length() - 1);
+    if (s.isEmpty()) {
+      return new LinkedList<>();
     }
+    String[] tempS = s.split(",");
 
+    Deque<Integer> arr = new LinkedList<>();
+
+    for (int i = 0; i < n; i++) {
+      arr.add(Integer.parseInt(tempS[i]));
+    }
+    return arr;
   }
-
 }
