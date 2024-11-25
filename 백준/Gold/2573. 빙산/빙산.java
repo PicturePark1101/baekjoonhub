21a1799c001e42f44eb0e6c01939533b9f104ccc
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-11-25 17:21:15 +0900

    [Gold IV] Title: 빙산, Time: 584 ms, Memory: 140300 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,131 +1,143 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Board {
-  private int r;
-  private int c;
-
-  public Board(int r, int c) {
-    this.r = r;
-    this.c = c;
-  }
-
-  public int getR() {
-    return r;
-  }
-
-  public int getC() {
-    return c;
-  }
-}
-
 public class Main {
+
   private static int N;
   private static int M;
-  private static int[][] board;
+  private static int[][] iceberg;
+  private static boolean[][] visited;
   private static int[] dx = {-1, 0, 0, 1};
   private static int[] dy = {0, 1, -1, 0};
 
+  static class Board {
+    private int r;
+    private int c;
+
+    public Board(int r, int c) {
+      this.r = r;
+      this.c = c;
+    }
+  }
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
+    iceberg = new int[N][M];
+    visited = new boolean[N][M];
 
-    board = new int[N][M];
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
       for (int j = 0; j < M; j++) {
-        board[i][j] = Integer.parseInt(st.nextToken());
+        iceberg[i][j] = Integer.parseInt(st.nextToken());
       }
     }
-
-    int count = 0;
-    int ans = 0;
-    while ((count = countSeparate()) < 2){ // 2보다 적으면 계속 녹인다.
-      if (count == 0) {
-        ans = 0;
-        break;
-      }
-      ans++; // ans 증가
-      melting();
-    }
-
-    System.out.println(ans);
-    br.close();
+    System.out.println(getAnswer());
   }
 
-  // 개수 구해
-  private static int countSeparate() {
+  private static int getAnswer() {
+    int year = 0;
+    while (!allFilledWithOne()) {
+      year++;
+      visited = new boolean[N][M];
+      for (int i = 1; i < N - 1; i++) {
+        for (int j = 1; j < M - 1; j++) {
+          if (iceberg[i][j] > 0 && !visited[i][j]) {
+            bfs(new Board(i, j));
+          }
+        }
+         
+      }
+        if (isAnswer()) return year;
+    }
+    return 0;
+  }
+
+  private static boolean isAnswer() {
     int count = 0;
-    boolean[][] visited = new boolean[N][M];
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
-        if (board[i][j] > 0 && !visited[i][j]) {
-          dfs(i, j, visited); // 방문
+    boolean[][] visitedForCount = new boolean[N][M];
+    for (int i = 1; i < N - 1; i++) {
+      for (int j = 1; j < M - 1; j++) {
+        if (iceberg[i][j] != 0 && !visitedForCount[i][j]) {
+          dfs(new Board(i, j), visitedForCount);
           count++;
         }
       }
     }
-    return count;
+    return count > 1;
   }
 
-  private static void melting() {
-    Queue<Board> queue = new LinkedList<>();
-    boolean[][] validIceberg = new boolean[N][M];
+  private static void dfs(Board current, boolean[][] visitedForCount) {
+    visitedForCount[current.r][current.c] = true;
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < 4; i++) {
+      int newR = current.r + dx[i];
+      int newC = current.c + dy[i];
+      if (iceberg[newR][newC] > 0 && !visitedForCount[newR][newC]) {
+        dfs(new Board(newR, newC), visitedForCount);
+      }
+    }
+  }
+  private static boolean allFilledWithOne() {
+    for (int i = 1; i < N - 1; i++) {
+      for (int j = 1; j < M - 1; j++) {
+        if (iceberg[i][j] != 0) return false;
+      }
+    }
+    return true;
+  }
+
+  private static void print() {
+    for (int i = 0; i < N ; i++) {
       for (int j = 0; j < M; j++) {
-        if (board[i][j] > 0) {
-          queue.offer(new Board(i, j));
-          validIceberg[i][j] = true;
-        }
+        System.out.print(iceberg[i][j]+" ");
+      }
+      System.out.println();
+    }
+  }
+  private static void bfs(Board start) {
+
+    Queue<Board> queue = new ArrayDeque<>();
+    queue.add(start);
+    visited[start.r][start.c] = true;
+    boolean[][] currentValidIceBege = new boolean[N][M];
+    for (int i = 1; i < N - 1; i++) {
+      for (int j = 1; j < M - 1; j++) {
+        if (iceberg[i][j] > 0) {
+          currentValidIceBege[i][j] = true;
+        };
       }
     }
 
     while (!queue.isEmpty()) {
-      Board b = queue.poll();
+      Board now = queue.poll();
+      int nowR = now.r;
+      int nowC = now.c;
       int count = 0;
+
       for (int i = 0; i < 4; i++) {
-        int newR = b.getR() + dx[i];
-        int newC = b.getC() + dy[i];
+        int newR = nowR + dx[i];
+        int newC = nowC + dy[i];
 
-        if (!isValid(newR, newC)) { // 유효한 값인지 검사하기
-          continue;
-        }
-        if (!validIceberg[newR][newC] && board[newR][newC] == 0) { // 0이면 증가시킴
+        if (!currentValidIceBege[newR][newC]) { // 녹아야하는 0임
           count++;
+        } else if(currentValidIceBege[newR][newC] && !visited[newR][newC]){
+          visited[newR][newC] = true;
+          queue.add(new Board(newR, newC));
         }
       }
-      if (board[b.getR()][b.getC()] - count < 0) {
-        board[b.getR()][b.getC()] = 0;
+
+      if (iceberg[nowR][nowC] - count < 0) {
+        iceberg[nowR][nowC] = 0;
       } else {
-        board[b.getR()][b.getC()] -= count;
+        iceberg[nowR][nowC] -= count;
       }
     }
-  }
-
-  private static void dfs(int r, int c, boolean[][] visited) {
-    visited[r][c] = true;
-    for (int i = 0; i < 4; i++) {
-      int newR = r + dx[i];
-      int newC = c + dy[i];
-
-      if (!isValid(newR, newC)) {
-        continue;
-      }
-      if (!visited[newR][newC] && board[newR][newC] > 0) { // 유효한 길이고, 0이 아니면 방문한다.
-        dfs(newR, newC, visited);
-      }
-    }
-  }
-
-  private static boolean isValid(int r, int c) {
-    return r >= 0 && r < N  && c >= 0 && c < M;
   }
 }
