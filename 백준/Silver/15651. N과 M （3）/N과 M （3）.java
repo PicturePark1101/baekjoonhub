9b2f09866893ca0819618faae37455ba297777ed
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-12-05 14:22:42 +0900

    [Silver III] Title: N과 M (3), Time: 408 ms, Memory: 68516 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  private static int N;
+  private static int M;
+  private static int[] bucket;
+  private static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    bucket = new int[M];
+
+    pick(0);
+    System.out.println(sb);
+    
+    br.close();
+  }
+
+  private static void pick(int depth) {
+    if (depth == M) {
+      for (int b : bucket) {
+        sb.append(b).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for (int i = 0; i < N; i++) {
+      bucket[depth] = i + 1;
+      pick(depth + 1);
+    }
+  }
+}
