0b7daee2a4c436c832695e358e97c4f8414078d1
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-12-09 15:45:07 +0900

    [Silver II] Title: 로또, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,47 +1,57 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-    private static int k;
-    private static int[] nums;
-    private static int[] bucket;
-    private static StringBuilder sb;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        bucket = new int[6];
-        k = Integer.parseInt(st.nextToken());
-        while(k != 0) {
-            nums = new int[k];
-            for (int i = 0; i < k; i++) {
-                nums[i] = Integer.parseInt(st.nextToken());
-            } 
-            makeLotto(0, 0, k);
-            sb.append("\n");
-            st = new StringTokenizer(br.readLine());
-            k = Integer.parseInt(st.nextToken()); 
-        }
-        
-        System.out.println(sb.toString());
-        br.close();
+  private static StringBuilder sb = new StringBuilder();
+  private static int[] bucket;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String input = br.readLine();
+    int N = Integer.parseInt(input.substring(0, 1));
+
+    while (N != 0) {
+      int[] nums = changeIntArr(input);
+      bucket = new int[6];
+
+      pick(nums, 0, 0);
+      sb.append("\n");
+      input = br.readLine();
+      N = Integer.parseInt(input.substring(0, 1));
     }
-    
-    private static void makeLotto(int start, int depth, int k) {
-        if(depth == 6) {
-            for (int i = 0; i < 6; i++) {
-                sb.append(bucket[i]+ " ");
-            }
-            sb.append("\n");
-            return;
-        }
-        
-        for (int i = start; i < k; i ++) {
-            bucket[depth] = nums[i];
-            makeLotto(i + 1, depth + 1, k);
-        }
+
+    System.out.println(sb);
+
+    br.close();
+  }
+
+  private static int[] changeIntArr(String s) {
+    StringTokenizer st = new StringTokenizer(s);
+    int N = Integer.parseInt(st.nextToken());
+    int[] nums = new int[N];
+    for (int i = 0; i < N; i++) {
+      nums[i] = Integer.parseInt(st.nextToken());
     }
+
+    Arrays.sort(nums);
+    return nums;
+  }
+
+  private static void pick(int[] nums, int start, int depth) {
+    if (depth == 6) {
+      for (int b : bucket) {
+        sb.append(b).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for (int i = start; i < nums.length; i++) {
+      bucket[depth] = nums[i];
+      pick(nums, i + 1, depth + 1);
+    }
+  }
 }
