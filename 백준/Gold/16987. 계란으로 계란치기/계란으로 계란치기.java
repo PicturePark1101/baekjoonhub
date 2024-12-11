5a987369eff0754cbc074fd1782f797f2c5c6fb8
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-12-11 19:22:40 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 164 ms, Memory: 14792 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -27,41 +27,36 @@ public class Main {
       st = new StringTokenizer(br.readLine());
       eggs[i] = new Egg(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
     }
-    hitEgg(0);
+    hitEgg(0, 0);
     System.out.println(answer);
     br.close();
   }
 
-  private static void hitEgg(int start) {
-    if (start == N) {
-      answer = Math.max(checkBrokenEggs(), answer);
+  private static void hitEgg(int start, int brokenCount) {
+    if (start == N || brokenCount == N - 1) {
+      answer = Math.max(brokenCount, answer);
       return;
     }
 
     Egg currentEgg = eggs[start];
-    for (int i = 0; i < N; i++) {
-      if(i != start) {
-        Egg nextEgg = eggs[i];
-        if (currentEgg.durability > 0 && nextEgg.durability > 0) {
-          nextEgg.durability -= currentEgg.weight;
-          currentEgg.durability -= nextEgg.weight;
-          hitEgg(start + 1);
-          nextEgg.durability += currentEgg.weight; // 계란 복구
-          currentEgg.durability += nextEgg.weight;
-        } else {
-          hitEgg(start + 1);
-        }
-      }
-    }
-  }
 
-  private static int checkBrokenEggs() {
-    int count = 0;
-    for (Egg e : eggs) {
-      if (e.durability <= 0) {
-        count++;
+    if (currentEgg.durability <= 0) {
+      hitEgg(start + 1, brokenCount);
+      return;
+    }
+
+    for (int i = 0; i < N; i++) {
+      Egg nextEgg = eggs[i];
+      if(i != start && nextEgg.durability > 0) {
+        nextEgg.durability -= currentEgg.weight;
+        currentEgg.durability -= nextEgg.weight;
+        int nextCount = brokenCount;
+        if (nextEgg.durability <= 0) nextCount++;
+        if (currentEgg.durability <= 0) nextCount++;
+        hitEgg(start + 1, nextCount);
+        nextEgg.durability += currentEgg.weight;
+        currentEgg.durability += nextEgg.weight;
       }
     }
-    return count;
   }
 }
