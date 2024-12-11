116bc2597b6bac1cdc5c7576a827e860d8cf75fd
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-12-11 18:02:05 +0900

    [Gold III] Title: 소문난 칠공주, Time: 256 ms, Memory: 83912 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,72 +1,83 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class Main {
-  private static boolean[] visited = new boolean[25];
-  private static int[] dx = {-1, 0, 0, 1};
-  private static int[] dy = {0, 1, -1, 0};
-  private static int ans = 0;
-  private static String[][] students = new String[5][5];
-  
-  public static void main(String[] args) throws IOException {
-    BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 
+  private static char[][] students;
+  private static int answer;
+  private static int[] dx = { -1, 0, 0, 1 };
+  private static int[] dy = { 0, 1, -1, 0 };
+  private static boolean[] visited;
+
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    students = new char[5][5];
+    visited = new boolean[25];
     for (int i = 0; i < 5; i++) {
-      students[i] = br.readLine().split("");
+      students[i] = br.readLine().toCharArray();
     }
 
-    dfs(0, 0, 0);
-    System.out.println(ans);
+    pick(0, 0, 0);
+    System.out.println(answer);
     br.close();
   }
 
-  private static void dfs(int depth, int start, int countY) {
-    if (countY >= 4) return;
+  private static void pick(int index, int depth, int yCnt) {
 
-    if (depth == 7){
-      int startIndex = start - 1;
-      if (isNearPath(startIndex / 5, startIndex % 5)) ans++;
+    if (yCnt >= 4) {
+      return;
+    }
+
+    if (depth == 7) {
+      int start = index - 1;
+      if (isNearPath(start / 5, start % 5)) {
+        answer++;
+      }
       return;
     }
 
 
-    for (int i = start; i < 25; i++) {
-      visited[i] = true;
-      if (students[i / 5][i % 5].equals("Y")) {
-        dfs(depth + 1, i + 1, countY + 1);
-      } else {
-        dfs(depth + 1, i + 1, countY);
+    for (int i = index; i < 25; i++) {
+      visited[i]= true;
+      if(students[i/5][i%5] == 'Y'){
+        pick(i + 1, depth + 1, yCnt + 1);
+      }else{
+        pick(i + 1, depth + 1, yCnt);
       }
       visited[i] = false;
     }
   }
 
   private static boolean isNearPath(int r, int c) {
-    Queue<Integer> queue = new LinkedList<>();
-    int count = 0;
-    boolean[] isVisitedQueue = new boolean[25];
+    Queue<Integer> queue = new ArrayDeque<>();
+    boolean[] visitedQueue = new boolean[25];
 
-    queue.offer(r * 5 + c);
-    isVisitedQueue[r * 5 + c] = true;
+    int count = 0;
+    int start = r * 5 + c;
+    queue.offer(start);
+    visitedQueue[start] = true;
+
     while (!queue.isEmpty()) {
-      int n = queue.poll();
+      int now = queue.poll();
       count++;
       for (int i = 0; i < 4; i++) {
-        int newR = dx[i] + (n / 5);
-        int newC = dy[i] + (n % 5);
-        if (isValid(newR, newC) && visited[newR * 5 + newC] && !isVisitedQueue[newR * 5 + newC]) {
-          isVisitedQueue[newR * 5 + newC] = true;
-          queue.offer(newR * 5 + newC);
+        int newR = (now / 5) + dx[i];
+        int newC = (now % 5) + dy[i];
+        int newIndex = newR * 5 + newC;
+        if (validPath(newR, newC) && !visitedQueue[newIndex] && visited[newIndex]) {
+          queue.offer(newIndex);
+          visitedQueue[newIndex] = true;
         }
       }
     }
     return count == 7;
   }
-
-  private static boolean isValid(int r, int c) {
+  private static boolean validPath(int r, int c) {
     return r >= 0 && r < 5 && c >= 0 && c < 5;
   }
 }
