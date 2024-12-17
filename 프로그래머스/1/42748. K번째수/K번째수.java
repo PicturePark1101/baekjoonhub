1bc303ac73f9b15f2384ee620585c20da094445e
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2024-12-17 15:08:07 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        
-        int[] answers = new int[commands.length];
-        int index = 0;
-        for (int m = 0; m < commands.length; m++) {
-            int i = commands[m][0];
-            int j = commands[m][1];
-            int k = commands[m][2];
-            
-            int[] list = Arrays.copyOfRange(array, i - 1, j);
-            Arrays.sort(list);
-            answers[index++] = list[k - 1];
+        int N = commands.length;
+        int M = commands[0].length;
+        int[] answer = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int[] newArr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(newArr);
+            answer[i] = pickK(newArr, commands[i][2]);
         }
-        return answers;
+
+        return answer;
+    }
+    
+    private int pickK(int[] arr, int k) {
+        return arr[k - 1];
     }
 }
