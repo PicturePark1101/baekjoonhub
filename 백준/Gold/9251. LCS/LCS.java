8b9a08172b1ecb5f5c9436a45e7eb5faaca65ee0
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-01-13 12:46:23 +0900

    [Gold V] Title: LCS, Time: 132 ms, Memory: 18544 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,26 +1,30 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str1 = br.readLine().split("");
-        String[] str2 = br.readLine().split("");
-        
-        int r = str1.length;
-        int c = str2.length;
-        int[][] dp = new int[r + 1][c + 1];
-        
-        for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= c; j++) {
-                if (str1[i - 1].equals(str2[j - 1])) { // str1의 i번째, str2의 j번째가 같으면 길이 갱신해야함
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // 다르면 이전에서 더 큰걸로 갱신      
-                }                               
-            }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str1 = br.readLine();
+    String str2 = br.readLine();
+    System.out.println(findAnswer(str1, str2));
+    br.close();
+  }
+
+  private static int findAnswer(String str1, String str2) {
+    int r = str1.length();
+    int c = str2.length();
+    int[][] DP = new int[r + 1][c + 1];
+    for (int i = 1; i <= r; i++) {
+      for (int j = 1; j <= c; j++) {
+        if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+          DP[i][j] = DP[i - 1][j - 1] + 1;
+        } else {
+          DP[i][j] = Math.max(DP[i - 1][j], DP[i][j - 1]);
         }
-        System.out.println(dp[r][c]);
-        br.close();
+      }
     }
+    return DP[r][c];
+  }
 }
