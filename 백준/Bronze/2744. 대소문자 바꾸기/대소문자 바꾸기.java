4b6f39b2aa4cb13c9fb8c408a36d954555f24401
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-02-11 15:38:45 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 108 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,24 +1,18 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-  public static void main(String[] args) throws Exception {
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String input = br.readLine();
-    String output = "";
-
-    for (int i = 0; i < input.length(); i++) {
-      char ch = input.charAt(i);
-      if (ch >= 'a' && ch <= 'z') {
-        output += Character.toString(ch - 32);
-      }else {
-        output += Character.toString(ch + 32);
-      }
-    }
-    System.out.println(output);
-
-  }
-
-}
+    public static void main(String[] args ) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] str = br.readLine().toCharArray();
+        
+        for (char ch : str) {
+            if (ch >= 'A' && ch <= 'Z') {
+               System.out.print(Character.toLowerCase(ch)); 
+            } else {
+                System.out.print(Character.toUpperCase(ch)); 
+            }
+        }
+        br.close();
+    }    
+} 
