bac7a0888e997950ae185353e519c58345c5922f
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-02-13 15:13:21 +0900

    [Silver V] Title: 줄세우기, Time: 172 ms, Memory: 17832 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,62 +1,43 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
-
-  private static int check(int[] p) {
-
-    int cnt = 0;
-    int[] newP = new int[20];
-
-    for (int i = 0; i < 20; i++) {
-      // i의 앞에 i보다 더 큰사람이 있는지를 검사
-
-      boolean flag = false;
-      for (int j = 0; j < i; j++) {
-        if (newP[j] > p[i]) {
-          // i가 j앞으로 가야함......
-          // 어떻게? 3중 포문? i를 j위치로, 나머지륾 위로
-          flag = true;
-          // j부터 기존꺼 뒤로 미루기
-          for (int k = i - 1; k >= j; k--) {
-            newP[k + 1] = newP[k];
-            cnt++;
-          }
-          newP[j] = p[i];
-          break;
-        }
-      }
-
-      if (!flag){
-        newP[i] = p[i];
-      }
-    }
-
-    return cnt;
-  }
   public static void main(String[] args) throws Exception {
-
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st;
+    int P = Integer.parseInt(br.readLine());
 
-    int n = Integer.parseInt(br.readLine());
+    StringBuilder sb = new StringBuilder();
 
-    for (int i = 0; i < n; i++) {
-      int[] people = new int[20];
+    for (int i = 1; i <= P; i++) {
       st = new StringTokenizer(br.readLine());
 
-      int order = Integer.parseInt(st.nextToken());
+      List<Integer> students = new ArrayList<>();
+      sb.append(st.nextToken()).append(" ");
 
-      for (int idx = 0; idx < 20; idx++) {
-        people[idx] = Integer.parseInt(st.nextToken());
+      int count = 0;
+
+      for (int j = 0; j < 20; j++) {
+        count += getLineStep(students, Integer.parseInt(st.nextToken()));
       }
-      bw.write(String.valueOf(i+1) +" "+String.valueOf(check(people)) +"\n");
+      sb.append(count).append("\n");
     }
-
-    bw.flush();
-    bw.close();
+    System.out.println(sb);
     br.close();
   }
 
+  private static int getLineStep(List<Integer> students, int newStudent) {
+    int step = 0;
+    for (int i = 0; i < students.size(); i++) {
+      if (students.get(i) > newStudent) {
+        step = students.size() - i;
+        students.add(i, newStudent);
+        return step;
+      }
+    }
+    students.add(newStudent);
+    return step;
+  }
+    
 }
