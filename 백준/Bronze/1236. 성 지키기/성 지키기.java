b952a11b7df3e84018ae3d9cc76a4a66a96e8e33
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-02-13 14:38:00 +0900

    [Bronze I] Title: 성 지키기, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.java
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -1,58 +1,43 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-  public static void main(String[] args) throws Exception {
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-
-    int r = Integer.parseInt(st.nextToken());
-    int c = Integer.parseInt(st.nextToken());
-
-
-    List<String>[] castle = new ArrayList[r];
-
-    for(int i = 0; i < r; i++) {
-      castle[i] = new ArrayList<String>();
-    }
-
-    for (int i = 0; i < r; i++) {
-      String input = br.readLine();
-      for (char ch : input.toCharArray()) {
-        castle[i].add(String.valueOf(ch));
-      }
-    }
-
-    // 행 확인
-    int cntR = 0;
-    for (int i = 0; i < r; i++) {
-      if (!castle[i].contains("X")){
-        cntR++;
-      }
-    }
-
-    // 열 확인
-    int cntC = 0;
-    for (int j = 0; j < c; j++) {
-      int flag = 1;
-      for (int i = 0; i < r; i++) {
-        if (castle[i].get(j).equals("X")) {
-          flag = 0;
-          break;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        String[] arr = new String[N];
+        
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
         }
-      }
-      if (flag == 1) {
-        cntC++;
-      }
-    }
 
-    bw.write(String.valueOf(Math.max(cntC, cntR)));
-
-    bw.close();
-    br.close();
-  }
-
+        int countR = 0;
+        for (int i = 0; i < N; i++) {
+            if (!arr[i].contains("X")) {
+                countR++;
+            }
+        }
+        
+        int countC = 0;
+        boolean flag;
+        for (int j = 0; j < M; j++) {
+            flag = true;
+            for (int i = 0; i < N; i++) {    
+                if (arr[i].charAt(j) == 'X') {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if (flag) {
+                countC++;
+            }
+        }
+        
+        System.out.println(Math.max(countC, countR));
+        br.close();
+    } 
 }
