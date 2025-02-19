26853df33883f733236cc9ba2453c405930a87e3
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-02-19 16:23:44 +0900

    [Silver V] Title: 회문인 수, Time: 128 ms, Memory: 18556 KB -BaekjoonHub

--- 백준/Silver/11068. 회문인 수/회문인 수.java
+++ 백준/Silver/11068. 회문인 수/회문인 수.java
@@ -2,50 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-            
-    public static ArrayList<Integer> makeBaseArr(int num, int B) {
-        ArrayList<Integer> baseList = new ArrayList<>();
-        
-        while (num > 0) {
-            baseList.add(num % B);
-            num /= B;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            sb.append(getAnswer(Integer.parseInt(br.readLine()))).append("\n");
         }
-       return baseList;
+        System.out.println(sb);
+        br.close();
     }
     
-    public static boolean isPalin(ArrayList<Integer> baseList) {
-        for (int i = 0; i < baseList.size() / 2; i++) {
-            if (baseList.get(i) != baseList.get(baseList.size() - i - 1)) {
-                return false;
-            }
+    private static int getAnswer(int n) {
+        for (int i = 2; i <= 64; i++) {
+            List<String> strs = new ArrayList<>();
+            rotation(strs, n, i);
+            
+            if (isPalin(strs)) { 
+                return 1 ;
+            };
+        }
+        return 0;
+    }
+    
+    private static boolean isPalin(List<String> strs) {
+        for (int i = 0; i < strs.size() / 2; i++) {
+            if (!strs.get(i).equals(strs.get(strs.size() - i - 1))) return false;
         }
         return true;
     }
     
-    public static void main(String[] args) throws Exception {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        
-        for (int i = 0; i < N; i++) {
-            boolean flag = false;
-            int input = Integer.parseInt(br.readLine());
-            for (int j = 2; j <= 64; j++) {
-                if (isPalin(makeBaseArr(input, j))) {
-                    flag = true;
-                    break;    
-                }
-            }
-            if (flag) {
-               sb.append("1");
-            } else {
-                sb.append("0");
-            }              
-            sb.append("\n");
+    private static void rotation(List<String> strs, int n, int b) {
+        if (n <= 0) {
+            return;
         }
-        System.out.println(sb);      
+        
+       strs.add(String.valueOf(n % b));
+       rotation(strs, n / b, b);
     }
+    
+    
 }
