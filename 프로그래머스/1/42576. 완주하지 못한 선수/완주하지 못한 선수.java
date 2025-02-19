7155d0f7546b49e6f512e83cef8b0af8cb15986c
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-02-19 18:08:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.42 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String, Integer> hm = new HashMap<>();
-
-        for (int i = 0; i < completion.length; i++) {
-            hm.put(completion[i], hm.getOrDefault(completion[i], 0) + 1);
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String people : participant) {
+            map.put(people, map.getOrDefault(people, 0) + 1);
         }
         
-        for (int i = 0; i < participant.length; i++) {
-            if (hm.get(participant[i]) == null || hm.get(participant[i]) == 0) {
-                return participant[i];
-            }
-            hm.put(participant[i], hm.get(participant[i]) - 1);                
+        for (String people : completion) {
+            map.put(people, map.get(people) - 1);
         }
-        return answer;
+        
+        for (Map.Entry<String, Integer> em : map.entrySet()) {
+            if (em.getValue() != 0) return em.getKey();
+        }
+        return "";
     }
 }
