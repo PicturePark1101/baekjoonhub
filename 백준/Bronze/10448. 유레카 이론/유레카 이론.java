230a51eba469729a8fdec9c9e9667bc310368829
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-02-19 15:03:56 +0900

    [Bronze I] Title: 유레카 이론, Time: 156 ms, Memory: 14988 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -1,44 +1,48 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-  private static int[] e = new int[45];
+  private static int T;
+  private static int[] tArr;
 
-  private static int eureka(int n) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
 
-    int cnt = 0;
-    for (int i = 1; i < e.length; i++) {
-      for (int j = 1; j < e.length; j++) {
-        for (int k = 1; k < e.length; k++) {
-          if (n == e[i] + e[j] + e[k])
+    tArr = new int[1001];
+    makeTArr();
+
+    T = Integer.parseInt(br.readLine());
+    for (int i = 0; i < T; i++) {
+      sb.append(getAnswer(Integer.parseInt(br.readLine()))).append("\n");
+    }
+
+    System.out.println(sb);
+    br.close();
+  }
+
+  private static void makeTArr() {
+    for (int i = 0; i < tArr.length; i++) {
+      tArr[i] = i * (i + 1) / 2;
+    }
+  }
+
+  private static int getAnswer(int n) {
+    int sum = 0;
+    for (int i = 1; i < tArr.length; i++) {
+      for (int j = 1; j < tArr.length; j++) {
+        for (int k = 1; k < tArr.length; k++) {
+          sum = tArr[i] + tArr[j] + tArr[k];
+          if (sum == n) {
             return 1;
+          } else if (sum > n) {
+            break;
+          }
         }
       }
     }
     return 0;
   }
-
-  private static void makeArr(){
-
-    for (int i = 1; i < e.length; i++) {
-      e[i] = i * (i + 1) / 2;
-    }
-  }
-
-  public static void main(String[] args) throws Exception{
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    makeArr();
-
-    int n = Integer.parseInt(br.readLine());
-    for (int i = 0; i < n; i++) {
-      bw.write(String.valueOf(eureka(Integer.parseInt(br.readLine())))+"\n");
-    }
-    bw.flush();
-    bw.close();
-    br.close();
-
-  }
-
 }
