bb3f2107e14be8dc9b898ffe968a46d60a1be5d9
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-02-24 14:47:58 +0900

    [Silver IV] Title: 행운의 바퀴, Time: 108 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.java
+++ 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.java
@@ -1,74 +1,68 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 
-  public static boolean makeWheel(String[] str, String s, int N, int cnt, int idx) {
-
-    if (isNotExist(str, s, idx)) { // 다른 곳에 존재하지 않음
-      if (str[idx].equals("?")) {
-        str[idx] = s;
-      } else if (!str[idx].equals(s)) {
-        return false;
-      }
-    } else {
-      return false;
-    }
-    return true;
-  }
-  public static boolean isNotExist(String[] strs, String s, int idx) {
-    for (int i = 0; i < strs.length; i++) {
-      if (i != idx && strs[i].equals(s)){
-
-        return false;
-      }
-    }
-    return true;
-
-  }
-  public static void main(String[] args) throws Exception {
+  private static int N;
+  private static int K;
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
 
-    int size = Integer.parseInt(st.nextToken());
-    int N = Integer.parseInt(st.nextToken());
-    String[] str = new String[size];
+    Deque<String> wheel = new ArrayDeque<>();
 
-    // 모두 ?로 초기화하기
-    for (int i = 0; i < str.length; i++) {
-      str[i] = "?";
+    for (int i = 0; i < N; i++) {
+      wheel.offer("?");
     }
 
-    int idx = 0;
     boolean flag = true;
-    for (int i = 0; i < N; i++) {
+
+    for (int i = 0; i < K; i++) {
       st = new StringTokenizer(br.readLine());
-      int cnt = Integer.parseInt(st.nextToken());
-      String s = st.nextToken();
+      int rotation = Integer.parseInt(st.nextToken());
+      String alpha = st.nextToken();
 
-      idx = (idx + cnt) % size;
-
-      if (!makeWheel(str, s, N, cnt, idx)){
-        bw.write("!");
+      if (!execute(rotation, wheel, alpha)) {
         flag = false;
         break;
       }
     }
 
     if (flag) {
-      for (int i = 0; i < size; i++) {
-        bw.write(str[idx]);
-        idx = (idx - 1 + size) % size;
-      }
+      printWheel(wheel);
+    } else {
+      System.out.println("!");
     }
-
-    bw.flush();
-    bw.close();
     br.close();
   }
 
+  private static void printWheel(Deque<String> wheel) {
+    System.out.print(wheel.poll());
+    while (!wheel.isEmpty()) {
+      System.out.print(wheel.pollLast());
+    }
+  }
+
+  private static boolean execute(int rotation, Deque<String> wheel, String alpha) {
+    for (int j = 0; j < rotation; j++) {
+      wheel.offer(wheel.poll());
+    }
+
+    String pointer = wheel.peek();
+    wheel.removeFirst();
+
+    if ((!pointer.equals("?")) && (!pointer.equals(alpha))) {
+      return false;
+    } else if (wheel.contains(alpha)) {
+      return false;
+    }
+    wheel.addFirst(alpha);
+    return true;
+  }
 }
