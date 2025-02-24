5cbc8ec39c66d425f9ff943c4294fcf7cb7d300d
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-02-24 15:44:50 +0900

    [Silver III] Title: ALPS식 투표, Time: 124 ms, Memory: 16116 KB -BaekjoonHub

--- 백준/Silver/2817. ALPS식 투표/ALPS식 투표.java
+++ 백준/Silver/2817. ALPS식 투표/ALPS식 투표.java
@@ -1,9 +1,8 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,52 +10,46 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-  public static void main(String[] args) throws Exception {
+  private static int X;
+  private static int N;
+  private static Map<String, Integer> staff = new HashMap<>();
+
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st;
-
-    int peopleNum = Integer.parseInt(br.readLine());
-    Map<Double, String> map = new HashMap<>();
-    Map<String, Integer> finalMap = new HashMap<>();
-
-    double line = peopleNum * 0.05;
-
-    int N = Integer.parseInt(br.readLine());
+    X = Integer.parseInt(br.readLine());
+    N = Integer.parseInt(br.readLine());
+    Map<Double, String> scoreMap = new HashMap<>();
 
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
+      String people = st.nextToken();
+      double score = Integer.parseInt(st.nextToken());
 
-      String p = st.nextToken();
-      int vote = Integer.parseInt(st.nextToken());
-
-      if (vote >= line) {
-        finalMap.put(p, 0);
-        for (double j = 1; j <= 14; j++) {
-          map.put(vote / j, p);
+      if (score >= X * 0.05) {
+        staff.put(people, 0);
+        for (int j = 1; j <= 14; j++) {
+          scoreMap.put(score / j, people);
         }
       }
     }
 
-    // 숫자들 내림차순으로 sort
-    List<Double> keySet = new ArrayList<>(map.keySet());
-    Collections.sort(keySet, Collections.reverseOrder());
+    List<Double> scores = new ArrayList<>(scoreMap.keySet());
+    scores.sort(Comparator.reverseOrder());
 
-    // 상위 14개 가져와서 finalMap에 저장
-    for (int j = 0; j < 14; j++) {
-      String p = map.get(keySet.get(j));
-      finalMap.replace(p, finalMap.get(p) + 1);
+    for (int i = 0; i < 14; i++) {
+      String people = scoreMap.get(scores.get(i));
+      staff.put(people, staff.getOrDefault(people, 0) + 1);
     }
-
-    List<String> finalList = new ArrayList<>(finalMap.keySet());
-    Collections.sort(finalList);
-    for(String s : finalList) {
-      bw.write(s+" "+finalMap.get(s) +"\n");
-    }
-
-    bw.flush();
-    bw.close();
+    print();
     br.close();
   }
 
+  private static void print() {
+    List<String> names = new ArrayList<>(staff.keySet());
+    names.sort(null);
+    for (String name : names) {
+      System.out.println(name+" "+staff.get(name));
+    }
+  }
 }
