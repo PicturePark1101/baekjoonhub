263e811c3cdd08e77b5a7118185975b6ab73d3b4
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-03-04 11:50:11 +0900

    [Silver III] Title: 빈도 정렬, Time: 120 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,45 +1,32 @@
-import java.io.*;
 import java.util.*;
-import java.util.Map.Entry;
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st;
-
-    Map<Integer, Integer> linkedHashMap = new LinkedHashMap<>();
-
-    st = new StringTokenizer(br.readLine());
-    int N = Integer.parseInt(st.nextToken());
-    int C = Integer.parseInt(st.nextToken());
-
-    st = new StringTokenizer(br.readLine());
-
-    for (int i = 0; i < N; i++) {
-      int num = Integer.parseInt(st.nextToken());
-      int cnt = linkedHashMap.getOrDefault(num, 0) + 1;
-      linkedHashMap.put(num, cnt);
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Map<Integer, Integer> countNums = new LinkedHashMap<>();
+        
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            countNums.put(num, countNums.getOrDefault(num, 0) + 1);
+        }
+        
+        List<Map.Entry<Integer, Integer>> mapToList = new ArrayList<>(countNums.entrySet());
+        Collections.sort(mapToList, (o1, o2) -> Integer.compare(o2.getValue(), o1.getValue()));
+        
+        for (Map.Entry<Integer, Integer> map : mapToList) {
+            for (int i = 0; i < map.getValue(); i++) {
+                sb.append(map.getKey()).append(" ");
+            }
+        }
+        System.out.println(sb);
+        br.close();     
     }
-
-    ArrayList<Map.Entry<Integer, Integer>> newList = new ArrayList<>(linkedHashMap.entrySet());
-
-    newList.sort(new Comparator<Entry<Integer, Integer>>() {
-      @Override
-      public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
-        return Integer.compare(o2.getValue(), o1.getValue());
-      }
-    });
-
-    for (Map.Entry<Integer, Integer> i : newList){
-      for (int j = 0; j < i.getValue(); j++) {
-        bw.write(String.valueOf(i.getKey())+" ");
-      }
-    }
-
-    bw.flush();
-    bw.close();
-    br.close();
-  }
 }
