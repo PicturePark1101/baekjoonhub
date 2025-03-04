c8b4865a2d0a0a754d024ab33867aa06c687c7d2
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-03-04 11:50:01 +0900

    [Silver II] Title: 좌표 압축, Time: 2204 ms, Memory: 281304 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,43 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
+    public static void main(String[] args) throws Exception {
 
-  public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st;
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] inputs = new int[N];
+        
+        Map<Integer, Integer> press = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            inputs[i] = n;
+            set.add(n);         
+        }
+        
+        List<Integer> list = new ArrayList<>(set);
 
-    ArrayList<Integer> originList = new ArrayList<>();
-
-    Map<Integer, Integer> linkedHashMap = new LinkedHashMap<>();
-    Set<Integer> set = new LinkedHashSet<>();
-
-    int N = Integer.parseInt(br.readLine());
-    st = new StringTokenizer(br.readLine());
-
-    for (int i = 0; i < N; i++) {
-      int num = Integer.parseInt(st.nextToken());
-      originList.add(num);
-      set.add(num);
-    }
-
-    ArrayList<Integer> newList = new ArrayList<>(set);
-    newList.sort(Comparator.naturalOrder());
-
-    for (int i = 0; i < newList.size(); i++) {
-      if (!linkedHashMap.containsKey(newList.get(i))) {
-        linkedHashMap.put(newList.get(i), i);
-      }
-    }
-
-    for (int i = 0; i < N; i++) {
-      bw.write(String.valueOf(linkedHashMap.get(originList.get(i)))+" ");
-    }
-
-    bw.flush();
-    bw.close();
+        list.sort(Comparator.naturalOrder());
+        for (int i = 0; i < list.size(); i++) {
+            press.put(list.get(i), i);
+        }
+        
+        printAnswer(inputs, press);
+      
     br.close();
   }
+    
+    private static void printAnswer(int[] inputs, Map<Integer, Integer> press) {
+        StringBuilder sb = new StringBuilder();
+        for (int n : inputs) {
+            sb.append(press.get(n)).append(" ");
+        }
+        System.out.println(sb);        
+    }
 
 }
