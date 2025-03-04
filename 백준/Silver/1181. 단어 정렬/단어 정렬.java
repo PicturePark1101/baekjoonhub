2603bd2ce7952a61ff04bfc87ee97e4376488474
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-03-04 10:15:08 +0900

    [Silver V] Title: 단어 정렬, Time: 332 ms, Memory: 26144 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,37 +1,35 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-
-    Set<String> wordSet = new HashSet<>();
-    for (int i = 0; i < N; i++) {
-      wordSet.add(br.readLine());
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());        
+        Set<String> strsSet = new HashSet<>();
+        
+        for (int i = 0; i < N; i++) {
+            strsSet.add(br.readLine());
+        }
+        
+        List<String> strs = new ArrayList<>(strsSet);
+        
+        
+        strs.sort((o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            } 
+            return o1.length() - o2.length();
+        });
+        
+        printStrs(strs);
+        br.close();
     }
-
-    List<String> words = new ArrayList<>(wordSet);
-    words = words.stream().sorted((o1, o2) -> {
-      if (o1.length() == o2.length()) {
-        return o1.compareTo(o2);
-      }
-      return Integer.compare(o1.length(), o2.length());
-    }).collect(Collectors.toList());
-    printWords(words);
-    br.close();
-  }
-
-  private static void printWords(List<String> words) {
-    StringBuilder sb = new StringBuilder();
-    for (String word : words) {
-      sb.append(word).append("\n");
+    
+    private static void printStrs(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str).append("\n");
+        }     
+        System.out.println(sb);
     }
-    System.out.println(sb);
-  }
 }
