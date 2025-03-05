acf9c725534a595bdbe9df6754c8bf1cacf0f777
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-03-05 14:55:23 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 984 ms, Memory: 123236 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,46 +1,53 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
-
 
 public class Main {
-
-  public static void main(String[] args) throws Exception {
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st;
-
-    st = new StringTokenizer(br.readLine());
-    int N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
-
-    int[][] acc = new int[N + 1][N + 1];
-    for (int i = 1; i <= N; i++) {
-      st = new StringTokenizer(br.readLine());
-      for (int j = 1; j <= N; j++) {
-        acc[i][j] = acc[i][j-1] + Integer.parseInt(st.nextToken());
-      }
+    private static int N;
+    private static int[][] count;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[][] arr = new int[N + 1][N + 1];
+        count = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }           
+        }
+        
+        makeCount(arr);
+        for (int i = 1; i <= M; i++) { 
+           st = new StringTokenizer(br.readLine()); 
+           int x1 = Integer.parseInt(st.nextToken());
+           int y1 = Integer.parseInt(st.nextToken());
+           int x2 = Integer.parseInt(st.nextToken()); 
+           int y2 = Integer.parseInt(st.nextToken()); 
+           sb.append(getAnswer(x1, y1, x2, y2)).append("\n");
+        }
+        System.out.println(sb);
+        
+        br.close();
     }
-
-    for (int i = 0; i < M; i++) {
-      int sum = 0;
-      st = new StringTokenizer(br.readLine());
-      int x1 = Integer.parseInt(st.nextToken());
-      int y1 = Integer.parseInt(st.nextToken());
-      int x2 = Integer.parseInt(st.nextToken());
-      int y2 = Integer.parseInt(st.nextToken());
-
-      for (int j = x1; j <= x2; j++) {
-        sum = sum + (acc[j][y2] - acc[j][y1-1]);
-      }
-
-      bw.write(String.valueOf(sum+"\n"));
+    
+    private static void makeCount(int[][] arr) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                count[i][j] = arr[i][j] + count[i][j - 1];             
+            }           
+        }        
     }
-
-    bw.flush();
-    bw.close();
-    br.close();
-
-  }
-
+    
+    private static int getAnswer(int x1, int y1, int x2, int y2) {
+        int sum = 0;
+        for (int i = x1; i <= x2; i++) {
+            sum += count[i][y2] - count[i][y1 - 1];
+        }
+        return sum;
+    }
 }
