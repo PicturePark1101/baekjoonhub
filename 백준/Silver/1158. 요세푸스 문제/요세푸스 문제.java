ee8ddbc9439fb8234696ba1c66447b4de5f6a6a3
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-03-31 15:15:45 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 548 ms, Memory: 294772 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -2,30 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        Queue<Integer> queue = new ArrayDeque<>();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
 
-        StringBuilder sb = new StringBuilder();
-        
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        
-        for (int i = 0; i < N; i++) {
-            queue.add(i + 1);
-        }
-        sb.append("<");
-        while(!queue.isEmpty()) {
-            for (int i = 0; i < K - 1; i++) {
-                queue.add(queue.poll());
-            }
-            sb.append(String.valueOf(queue.poll()));
-            if (queue.size() != 0) {
-                sb.append(", ");
-            }
-        }
-        sb.append(">");
-        System.out.println(sb);
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+
+    Queue<Integer> people = new LinkedList<>();
+    for (int i = 1; i <= N; i++) {
+      people.add(i);
     }
+    int count = 0;
+
+    sb.append("<");
+    while(people.size() != 1) {
+      count++;
+      if (count == K) {
+        sb.append(people.poll()).append(", ");
+        count = 0;
+      } else {
+        people.add(people.poll());
+      }
+    }
+    sb.append(people.poll()).append(">");
+    System.out.println(sb);
+    br.close();
+  }
 }
