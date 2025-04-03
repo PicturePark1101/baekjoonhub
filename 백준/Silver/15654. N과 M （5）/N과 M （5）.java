279a48c378eafca701e4efae3eb5e1864c5a2898
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-04-03 15:59:28 +0900

    [Silver III] Title: N과 M (5), Time: 288 ms, Memory: 28612 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,55 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-  private static int N;
-  private static int M;
-  private static int[] bucket;
-  private static int[] nums;
-  private static boolean[] visited;
-
-  private static StringBuilder sb = new StringBuilder();
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    nums = new int[N];
-    bucket = new int[M];
-    visited = new boolean[N];
-
-    st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < N; i++) {
-      nums[i] = Integer.parseInt(st.nextToken());
+    private static int N;
+    private static int M;
+    private static int[] arr;
+    private static int[] bucket;
+    private static boolean[] visited;
+    private static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));           
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); 
+        M = Integer.parseInt(st.nextToken()); 
+        
+        arr = new int[N];
+        visited = new boolean[N];
+        bucket = new int[M];
+        
+        st = new StringTokenizer(br.readLine());        
+        for (int i = 0 ; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        
+        permutation(0);
+        System.out.print(sb);
+        br.close();
     }
-    Arrays.sort(nums);
-
-    pick(0);
-    System.out.println(sb);
-    br.close();
-  }
-
-  private static void pick(int depth) {
-    if (depth == M) {
-      for (int b : bucket) {
-       sb.append(b).append(" ");
-      }
-      sb.append("\n");
-      return;
+    
+    private static void permutation(int r) {
+        if (r == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(bucket[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                bucket[r] = arr[i];
+                permutation(r + 1);
+                visited[i] = false;
+            }
+        }
+        
     }
-
-    for (int i = 0; i < N; i++) {
-      if (visited[i]) continue;
-      visited[i] = true;
-      bucket[depth] = nums[i];
-      pick(depth + 1);
-      visited[i] = false;
-    }
-  }
 }
