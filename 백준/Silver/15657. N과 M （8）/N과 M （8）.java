a44181d06d3f3ce750ad46a91da2135342845767
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-04-03 16:27:50 +0900

    [Silver III] Title: N과 M (8), Time: 136 ms, Memory: 18116 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,49 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    
+    private static int N;
+    private static int M;
+    private static int[] nums;
+    private static int[] bucket;
+    private static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-  private static int N;
-  private static int M;
-  private static int[] bucket;
-  private static int[] nums;
-  private static StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        bucket = new int[M];
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    nums = new int[N];
-    bucket = new int[M];
-
-    st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < N; i++) {
-      nums[i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+          nums[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(nums);
+        pick(0, 0);
+        
+        System.out.println(sb);
+        br.close();
     }
-    Arrays.sort(nums);
-    pick(0, 0);
-
-    System.out.println(sb);
-    br.close();
-  }
-
-  private static void pick(int start, int depth) {
-    if (M == depth) {
-      for (int b : bucket) {
-        sb.append(b).append(" ");
-      }
-      sb.append("\n");
-      return;
+    
+    private static void pick(int start, int depth) {
+        if (M == depth) {
+            for (int b : bucket) {
+                sb.append(b).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = start; i < N; i++) {
+            bucket[depth] = nums[i];
+            pick(i, depth + 1);
+        }
     }
-
-    for (int i = start; i < N; i++) {
-      bucket[depth] = nums[i];
-      pick(i, depth + 1);
-    }
-  }
+    
 }
