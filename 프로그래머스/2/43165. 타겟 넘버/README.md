d64f6dbce0126a478aeedf4f9b9ba34ad5a282e5
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-04-07 18:02:56 +0900

    [level 2] Title: 타겟 넘버, Time: 9.79 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 6.68 ms
+메모리: 76.4 MB, 시간: 9.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 19일 20:23:14
+2025년 04월 07일 18:02:54
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,28 @@
 class Solution {
     
-    private int answer = 0;
+    private int[] op = { -1,  1 };
+    private int N;
+    private int T;
+    private int answer;
     
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
-        return answer;
-        
+        N = numbers.length;
+        T = target;
+        return dfs(numbers, 0, 0);
     }
     
-    private void dfs(int[] numbers, int target, int depth, int sum) {
-        if (depth == numbers.length) {
-            if (target == sum) answer++;
-            return;
+    // 중복을 허용함. 2개를 5번에 
+    
+    private int dfs(int[] numbers, int depth, int n) {
+        if (depth == N) {
+            if (n == T) {
+                return 1;
+            } 
+            return 0;
         }
-        
-        dfs(numbers, target, depth + 1, sum + numbers[depth]);
-        dfs(numbers, target, depth + 1, sum + numbers[depth] * -1);
+               
+        return dfs(numbers, depth + 1, n + numbers[depth] * op[0]) + dfs(numbers, depth + 1, n + numbers[depth] * op[1]);
     }
+    
+    
 }
