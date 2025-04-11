7772434ffad77ea9d64cd6ef00460fb4359cbcb4
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-04-11 16:23:52 +0900

    [Silver II] Title: 부분수열의 합, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,39 +1,40 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-    private static int N;
-    private static int S;
-    private static int[] nums;
-    private static int ans = 0;
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        nums = new int[N];
-      
-        st = new StringTokenizer(br.readLine());        
-        for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        makeAns(0, 0);
-        if (S == 0) ans--;
-        System.out.println(ans);
-        br.close();
-    }
-    
-    private static void makeAns(int depth, int sum) {
-        if (depth == N) {
-            if (sum == S) { ans++; }
-            return;
-        }
-        
-        makeAns(depth + 1, sum + nums[depth]);
-        makeAns(depth + 1, sum);
-    }
+
+	private static int[] nums;
+	private static int N;
+	private static int S;
+	private static int answer;
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		nums = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		dfs(0, 0);
+		if (S == 0) answer--;
+		System.out.println(answer);
+		br.close();
+	}
+
+	private static void dfs(int depth, int sum) {
+		if (depth == N) {
+			if (sum == S) answer++;
+			return;
+		}
+		dfs(depth + 1, sum + nums[depth]);
+		dfs(depth + 1, sum);
+
+	}
 }
