8a70818b97df16c99503f8bdb8a383213caa7247
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-04-11 15:48:35 +0900

    [Gold IV] Title: 회사 문화 1, Time: 532 ms, Memory: 77180 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -1,63 +1,60 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static List<Integer>[] tree;
+	private static int[] priceCount;
+	private static boolean[] visited;
 
-  static List<Integer>[] childEmployee;
-  static int[] praise;
-  static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-  public static void main(String[] args) throws Exception {
+		tree = new List[N + 1];
+		priceCount = new int[N + 1];
+		visited = new boolean[N + 1];
+		for (int i = 0; i <= N; i++) {
+			tree[i] = new ArrayList<>();
+		}
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			if (n == -1) continue;
+			tree[n].add(i);
+		}
 
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
 
-    praise = new int[n + 1];
-    visited = new boolean[n + 1];
-    childEmployee = new ArrayList[n + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			priceCount[n] += w;
+		}
 
-    for (int i = 1; i <= n; i++) {
-      childEmployee[i] = new ArrayList<>();
-    }
+		dfs(1);
 
-    st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			sb.append(priceCount[i]).append(" ");
+		}
+		System.out.println(sb);
+		br.close();
+	}
 
-    for (int i = 1; i <= n; i++) {
-      int employee = Integer.parseInt(st.nextToken());
-      if (i == 1) continue;
-      childEmployee[employee].add(i);
-    }
+	private static void dfs(int n) {
+		visited[n] = true;
 
-    for (int i = 1; i <= m; i++) {
-      st = new StringTokenizer(br.readLine());
-      praise[Integer.parseInt(st.nextToken())] += Integer.parseInt(st.nextToken());
-    }
-
-    bfs(1);
-
-    for (int i = 1; i <= n; i++) {
-      System.out.print(praise[i]+" ");
-    }
-  }
-
-  public static void bfs(int start) {
-
-    Queue<Integer> qu = new LinkedList<>();
-    qu.offer(start);
-    while (!qu.isEmpty()) {
-      int poll = qu.poll();
-      for (int n : childEmployee[poll]) {
-        qu.add(n);
-        praise[n] += praise[poll];
-      }
-    }
-  }
+		for (int t : tree[n]) {
+			if(visited[t]) continue;
+			priceCount[t] += priceCount[n];
+			dfs(t);
+		}
+	}
 }
