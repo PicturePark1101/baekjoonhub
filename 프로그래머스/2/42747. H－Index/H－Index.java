428ce8c688e8041cfb62e8a1df081c70b4dc9004
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-04-19 15:22:32 +0900

    [level 2] Title: H-Index, Time: 1.53 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,19 +1,48 @@
 import java.util.*;
 
 class Solution {
+    private int N;
+    private int[] arr;
     
     public int solution(int[] citations) {
-        Arrays.sort(citations);
-        int answer = 0;
-        for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i; // 인용된 논문의 수
-			
-			if(citations[i] >= h) {
-				answer = h;
-				break;
-			}
+        arr = citations;
+        N = citations.length;
+        Arrays.sort(arr);
+        int max = arr[N - 1];
+        
+        for (int i = max; i >= 0; i--) {
+            if (isValid(i)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+    
+    
+    private boolean isValid(int h) {   
+        // h편 이상인 가장 작은 논문을 찾는다. 
+        int index = findIdx(h); 
+        int h1 = N - index;  
+        return h1 == h;
+    }
+    
+    private int findIdx(int h) {
+        int p = 0;
+        int q = N - 1;
+        int answer = N;
+        
+        while (p <= q) {
+            int mid = (p + q) / 2;
+            
+            if (h > arr[mid]) {
+                p = mid + 1;
+            } else if (h <= arr[mid]) {
+                answer = mid;
+                q = mid - 1;
+            }        
         }
         return answer;
     }
+    
     
 }
