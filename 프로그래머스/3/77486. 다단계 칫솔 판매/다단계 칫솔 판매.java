7e8171ac5c93e517e0b9c63cc45104177f2aaf5c
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-04-30 15:11:17 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 63.52 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,44 +1,57 @@
 import java.util.*;
 
 class Solution {
-    private final String root = "MINHO";
-    private HashMap<String, String> referralMap = new HashMap<>();
-    private HashMap<String, Integer> resultMap = new HashMap<>();
-
+    
+    static class Employees {
+        private String name;
+        private Employees recommand;
+        public Employees(String name) {
+            this.name = name;
+        }
+    }
+    
+    private Map<String, Integer> income = new HashMap<>();
+    private int N;
+    private Map<String, String> employee = new HashMap<>();
+    private int M;
+    
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int N = enroll.length;
+        N = enroll.length;
+        M = seller.length;
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            employee.put(enroll[i], referral[i]);
+            income.put(enroll[i], 0);
+        }
+
+        double b = 12 * 0.1;
+        System.out.println((int) b);
+        
+        for (int i = 0; i < M; i++) { 
+            calc(seller[i], amount[i] * 100);
+        }
         
         for (int i = 0; i < N; i++) {
-            if (referral[i].equals("-")) {
-                referralMap.put(enroll[i], root);
-            }else {
-                referralMap.put(enroll[i], referral[i]);
-            }
+            answer[i] = income.get(enroll[i]);
         }
         
-        for (int i = 0; i < seller.length; i++) {
-            calcResult(seller[i], amount[i] * 100);
-        }
-        
-        int[] answer = new int[N];
-        for (int i = 0; i < N; i++) { 
-            answer[i] = resultMap.getOrDefault(enroll[i], 0);
-        }
         return answer;
     }
     
-    private void calcResult(String currentSeller, int money) {
-        if (currentSeller.equals(root)) {
-            return;
-        }
-
-        if (money * 0.1 < 1) {
-            resultMap.put(currentSeller, resultMap.getOrDefault(currentSeller, 0) + money);
+    private void calc(String emp, int money) {
+        if (emp.equals("-")) return;
+        
+        double remain = money * 0.1;
+        
+        if ((int) remain < 1) {
+            income.put(emp, income.get(emp) + money);
             return;
         }
         
-        resultMap.put(currentSeller, resultMap.getOrDefault(currentSeller, 0) + money - (money / 10));
+        int newM = money - (int) remain;
         
-        calcResult(referralMap.get(currentSeller), money / 10);
+        income.put(emp, income.get(emp) + (int)newM);
+        // System.out.println(emp+" "+income.get(emp) +" "+(int)remain);
+        calc(employee.get(emp), (int)remain);
     }
 }
