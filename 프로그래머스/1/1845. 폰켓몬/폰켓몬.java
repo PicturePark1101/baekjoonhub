53871911a85b16c92d4011585b54142ad6a41456
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-05-13 14:22:03 +0900

    [level 1] Title: 폰켓몬, Time: 5.06 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,16 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0) + 1);
+        Set<Integer> peonKet = new HashSet<>();
+        for (int n : nums) {
+            peonKet.add(n);
         }
-        System.out.println(hm.size());
-        
-        if (hm.size() > nums.length / 2) {
-            return nums.length / 2;
+        if (peonKet.size() > nums.length / 2) {
+            answer = nums.length / 2;
+        } else {
+            answer = peonKet.size();
         }
-        return hm.size();
+        return answer;
     }
 }
