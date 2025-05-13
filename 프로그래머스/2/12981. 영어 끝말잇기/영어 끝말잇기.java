a1a4d0fcf8ce984b65fa966e63d474406abe407e
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-05-13 14:49:46 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,22 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {0 ,0};
+
+        Set<String> wordSet = new HashSet<>();
+    
         
-        HashSet<String> wordSet = new HashSet<>();
-        String beforeWord = words[0];
+        char lastAlphabet = words[0].charAt(words[0].length() - 1);
         wordSet.add(words[0]);
+
         for (int i = 1; i < words.length; i++) {
-            char beforeLastChar = beforeWord.charAt(beforeWord.length() - 1);
-            char currentFirstChar = words[i].charAt(0);
-
-            if (beforeLastChar != currentFirstChar || wordSet.contains(words[i])) {
-                return new int[]{i % n + 1, i / n + 1};
+            int id = i % n;
+            // 탈락 조건
+            char startAlphabet = words[i].charAt(0);
+            
+            if (startAlphabet != lastAlphabet || wordSet.contains(words[i])) {
+                return new int[]{id + 1, i / n + 1};
             }
-            wordSet.add(words[i]);
-            beforeWord = words[i];
+            
+            wordSet.add(words[i]);  
+            lastAlphabet = words[i].charAt(words[i].length() - 1);
+            
         }
-
-        return answer;
+        return new int[]{0, 0};
     }
 }
