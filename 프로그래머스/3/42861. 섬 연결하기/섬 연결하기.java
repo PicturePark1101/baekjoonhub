76f574aa25edd596ccc49cffa9fb4945d3b7a470
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-05-14 16:31:26 +0900

    [level 3] Title: 섬 연결하기, Time: 0.86 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,42 +1,53 @@
 import java.util.*;
-class Solution {
-    private static int[] parent;
-    public int solution(int n, int[][] costs) {
-        Arrays.sort(costs, (o1, o2) -> Integer.compare(o1[2], o2[2]));
-        parent = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-        
-        int edages = 0;
-        int answer = 0;
 
-        for (int[] edge : costs) {
-            if (edages == n - 1) {
-                break;
-            }
+class Solution {
+    private int N;
+    private int[] graph;
+    
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        N = costs.length;
+        graph = new int[n];
+        int edge = 0;
+        initGraph();
         
-            if (find(edge[0]) != find(edge[1])) { // 루트가 같지 않다면
-                union(edge[0], edge[1]);
-                answer += edge[2];
-                edages++;
-            }
-        }
+        Arrays.sort(costs, (a, b) -> {
+            return a[2] - b[2];
+        });
+        
+        for (int i = 0; i < N; i++) {
             
+            if (edge == n - 1) return answer;
+            
+            if (isCycle(costs[i][0], costs[i][1])) continue;
+            
+            union(costs[i][0], costs[i][1]);
+            answer += costs[i][2];
+            edge++;
+        }
         return answer;
     }
-    
-    private static void union(int n1, int n2) {
-        int root1 = find(n1);
-        int root2 = find(n2); 
-        parent[root2] = root1;
+   
+    private boolean isCycle(int a, int b) {
+        int rootA = findRoot(a);
+        int rootB = findRoot(b);
+        return rootA == rootB;
     }
     
-    private static int find(int x) {
-        if (parent[x] == x) {
-            return x;
+    private int findRoot(int n) {
+        if (graph[n] == n) return n;
+        return findRoot(graph[n]); 
+    }
+    
+    private void union(int a, int b) {
+        int rootA = findRoot(a);
+        int rootB = findRoot(b);
+        graph[rootA] = rootB;
+    }
+    
+    private void initGraph() {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = i;
         }
-        return parent[x] = find(parent[x]);
     }
 }
