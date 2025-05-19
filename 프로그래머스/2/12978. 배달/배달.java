ca06692974d25a226234ffd11b7ff87c2a0d8595
박서진 <109809242+PicturePark1101@users.noreply.github.com>
2025-05-19 13:05:39 +0900

    [level 2] Title: 배달, Time: 4.08 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,65 +1,64 @@
 import java.util.*;
 
 class Solution {
-
+    
     static class Node {
         private int dest;
         private int cost;
-        
         public Node(int dest, int cost) {
             this.dest = dest;
             this.cost = cost;
         }
     }
     
-    private ArrayList<Node>[] adList;
     private int[] dist;
+    private List<Node>[] adjust;
     
     public int solution(int N, int[][] road, int K) {
-        
-        adList = new ArrayList[N + 1];
-        for (int i = 0; i <= N; i++) {
-            adList[i] = new ArrayList<>();
-        }
-        
-        for (int i = 0; i < road.length; i++) {
-            int a = road[i][0];
-            int b = road[i][1];
-            int c = road[i][2];            
-            adList[a].add(new Node(b, c));
-            adList[b].add(new Node(a, c));
-        }
-        
         int answer = 0;
+
+        dist = new int[N + 1];
+        adjust = new List[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            adjust[i] = new ArrayList<>();
+        }
+        makeAdjust(road);
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        makeDist();
         
-        dijkstra(N);
-        
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N + 1; i++) {
             if (dist[i] <= K) answer++;
         }
         return answer;
     }
     
-    private void dijkstra(int N) {
-        dist = new int[N + 1];
+    private void makeDist() {
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.cost, b.cost));
         
-        Arrays.fill(dist, Integer.MAX_VALUE);  
+        pq.add(new Node(1, 0));
         dist[1] = 0;
         
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
         
-        pq.add(new Node(1, 0));          
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();     
-            if (dist[now.dest] < now.cost) continue;
+            if (dist[now.dest] < now.cost) {
+                continue;
+            }
             
-            for (Node next : adList[now.dest]) {
-                if (next.cost + now.cost < dist[next.dest]) {
-                    dist[next.dest] = next.cost + now.cost;
+            for (Node next : adjust[now.dest]) {
+                if (dist[next.dest] > next.cost + now.cost ) {
+                    dist[next.dest] = next.cost + now.cost; 
                     pq.add(new Node(next.dest, dist[next.dest]));
                 }
             }
-        }        
+        }
+        
+    }
+    
+    private void makeAdjust(int[][] road) {
+        for (int i = 0; i < road.length; i++) {
+            adjust[road[i][0]].add(new Node(road[i][1], road[i][2]));
+            adjust[road[i][1]].add(new Node(road[i][0], road[i][2]));
+        }
     }
 }
